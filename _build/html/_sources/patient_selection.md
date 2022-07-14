2528d23cfb5a887bcac2e6a3becde129eaf36c37
Jacob McLaughlin <pc20455@bristol.ac.uk>
2022-07-14 23:38:14 +0100

    Correcting formatting issue

--- _build/html/_sources/patient_selection.md
+++ _build/html/_sources/patient_selection.md
@@ -1,4 +1,3 @@
-patient
 ---
 jupytext:
   cell_metadata_filter: -all
---
+++ _build/jupyter_execute/patient_selection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Patient Selection
+# 
+# This page will contain information on the patient selection criteria for the study. 
+# 
+# ## Analysis
+# 
+# It also contains room for supporting analyis in python.
+
+# In[1]:
+
+
+print("Placeholder")
+
